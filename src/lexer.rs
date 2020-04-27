05d0576916aa593fe75c2e8d9e094b0c90cf89f0
Bartosz Marczyński <marczynski.bartosz@gmail.com>
2020-04-27 22:14:49 +0200

    add basic lexer

---
+++ src/lexer.rs
@@ -0,0 +1,75 @@
+use std::collections::VecDeque;
+use std::iter::Peekable;
+use std::str::Chars;
+
+#[derive(Debug)]
+pub enum Token {
+    Symbol(String),
+    String(String),
+    Quote,
+    LParen,
+    RParen,
+}
+
+#[derive(Debug)]
+pub enum LexerError {
+    Reason(String),
+}
+
+pub fn tokenize(code: String) -> Result<VecDeque<Token>, LexerError> {
+    let mut chars = code.chars().peekable();
+    let mut tokens: VecDeque<Token> = VecDeque::new();
+    while let Some(c) = chars.peek() {
+        if c == &'(' {
+            tokens.push_back(Token::LParen);
+            chars.next();
+            continue;
+        } else if c == &')' {
+            tokens.push_back(Token::RParen);
+            chars.next();
+            continue;
+        } else if c == &'\'' {
+            tokens.push_back(Token::Quote);
+            chars.next();
+            continue;
+        } else if c == &'\"' {
+            chars.next();
+            let token = tokenize_string(&mut chars)?;  
+            tokens.push_back(token);
+            continue;
+
+        } else if (&c).is_whitespace() {
+            chars.next();
+            continue;
+        }
+        
+        let token = tokenize_symbol(&mut chars)?;
+        tokens.push_back(token);
+    }
+
+    Ok(tokens)
+}
+
+fn tokenize_symbol(code: &mut Peekable<Chars>) -> Result<Token, LexerError> {
+    let mut symbol = String::new();
+    while let Some(c) = code.next() {
+        if c.is_whitespace() {
+            return Ok(Token::Symbol(symbol));
+        }
+        symbol.push(c)
+    }
+
+    Ok(Token::Symbol(symbol))
+}
+
+fn tokenize_string(code: &mut Peekable<Chars>) -> Result<Token, LexerError> {
+    let mut string = String::new();
+    while let Some(c) = code.next() {
+        if c == '\"' {
+            return Ok(Token::String(string));
+        }
+        string.push(c)
+    }
+
+    Err(LexerError::Reason("missing closing quote".to_owned()))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use std::collections::VecDeque;
 use std::io;
-use std::iter::Peekable;
-use std::str::Chars;
+mod lexer;
+use lexer::tokenize;
 
 fn main() {
-    println!("{:?}", tokenize("(\"Hello, world!\")".to_owned()));
+    println!("{:?}", tokenize("(dudu \"Hello, world!\")".to_owned()));
 }
 
 #[allow(dead_code)]
@@ -16,64 +15,4 @@ fn slurp_exp() -> String {
     expr
 }
 
-#[derive(Debug)]
-pub enum Token {
-    Symbol(String),
-    String(String),
-    Quote,
-    LParen,
-    RParen,
-}
-
-#[derive(Debug)]
-pub enum LexerError {
-    Reason(String),
-}
-
-pub fn tokenize(code: String) -> VecDeque<Token> {
-    let mut chars = code.chars().peekable();
-    let mut tokens: VecDeque<Token> = VecDeque::new();
-    while let Some(c) = chars.peek() {
-        if c == &'('  {
-            tokens.push_back(Token::LParen);
-            chars.next();
-            continue;
-        } else if c == &')' {
-            tokens.push_back(Token::RParen);
-            chars.next();
-            continue;
-        } else if c == &'\"' {
-            chars.next();
-            tokens.push_back(tokenize_string(&mut chars).unwrap())
-        }
-
-    }
-
-    tokens
-}
-
-#[allow(dead_code)]
-fn tokenize_symbol(code: &mut Peekable<Chars>) -> Result<Token, LexerError> {
-    let mut symbol = String::new();
-    while let Some(c) = code.next() {
-        if c.is_whitespace() {
-            return Ok(Token::Symbol(symbol));
-        }
-        symbol.push(c)
-    }
-    Err(LexerError::Reason("dunno mate".to_owned()))
-}
-
-#[allow(dead_code)]
-fn tokenize_string(code: &mut Peekable<Chars>) -> Result<Token, LexerError> {
-    let mut string = String::new();
-    while let Some(c) = code.next() {
-        if c == '\"' {
-            return Ok(Token::String(string));
-        }
-        string.push(c)
-    }
-
-    Err(LexerError::Reason("missing closing quote".to_owned()))
-}
 
